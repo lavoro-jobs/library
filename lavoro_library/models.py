7b0f9841a8843960df8b631dd5e6184ac0174f25
Marko Bolt <marko.bolt@fer.hr>
2023-10-30 20:11:19 +0100

    created catalog models

--- lavoro_library/models.py
+++ lavoro_library/models.py
@@ -24,3 +24,29 @@ class UserInDB(User):
     id: uuid.UUID
     password_hash: str
     created_on: datetime
+
+
+class PositionCatalog(BaseModel):
+    id: int
+    position_name: str
+
+
+class EducationCatalog(BaseModel):
+    id: int
+    education_level: str
+
+
+class ContractTypeCatalog(BaseModel):
+    id: int
+    contract_type: str
+
+
+class WorkTypeCatalog(BaseModel):
+    id: int
+    work_type: str
+
+
+class SkillsCatalog(BaseModel):
+    id: int
+    skill_name: str
+
