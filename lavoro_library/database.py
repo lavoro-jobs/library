fe995d13b0a2af01ae1b21b5068cb54e46ebde96
Jakov Vinožganić <yourgitemail@example.com>
2023-11-02 18:22:34 +0100

    Fixed execute functions

--- lavoro_library/database.py
+++ lavoro_library/database.py
@@ -1,7 +1,7 @@
 import sys
 import time
-import psycopg2
-import psycopg2.extras
+import psycopg
+from psycopg.rows import dict_row  
 
 
 class Database:
@@ -11,16 +11,18 @@ class Database:
     def connect(self, connection_string, max_retries=3):
         for i in range(0, max_retries):
             try:
-                connection = psycopg2.connect(connection_string)
+                connection = psycopg.connect(connection_string, row_factory=dict_row)
+                # connection.autocommit = True
                 return connection
-            except psycopg2.OperationalError as e:
+            except psycopg.OperationalError as e:
                 print("Unable to connect to config database. Retrying...")
                 time.sleep(3)
         print("Unable to connect to config database")
         sys.exit(1)
 
-    def execute_query(self, query, params=None):
-        with self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+    def execute_one(self, query, params=None):
+        cursor = self.connection.cursor()
+        with self.connection.transaction():
             cursor.execute(query, params)
 
             if cursor.description:
@@ -29,9 +31,11 @@ class Database:
             else:
                 return {"affected_rows": cursor.rowcount}
 
-    def execute_query_batch(self, query, params=None):
-        with self.connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as cursor:
-            # TODO: handle returning results like in execute_query()
-            cursor.executemany(query, params)
-            result = cursor.fetchall()
-            return {"result": result, "affected_rows": cursor.rowcount}
+    def execute_many(self, query_params_list):
+        cursor = self.connection.cursor()
+        with self.connection.transaction():
+            for query, params in query_params_list:
+                cursor.execute(query, params)
+
+            return {"affected_rows": cursor.rowcount}
+
