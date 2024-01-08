cabbb67976de3e281e064a64998061432703da09
Marko Bolt <marko.bolt@fer.hr>
2024-01-08 15:37:11 +0100

    fix

--- pre_install.sh
+++ pre_install.sh
@@ -1,2 +1,2 @@
-pip3 install pydantic[email] psycopg[binary] python-multipart pika jsonpickle fastapi fastapi-mailer[smtp] uvicorn
+pip3 install pydantic[email] psycopg[binary] python-multipart fastapi-mail pika jsonpickle fastapi
 apk add libpq
